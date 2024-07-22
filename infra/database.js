bb3c49cde068668742aebbc747e52e863730029f
ianmateusES <estudo.ianmateus@gmail.com>
2024-07-22 04:03:39 +0000

    feat: config database

---
+++ infra/database.js
@@ -0,0 +1,23 @@
+import { Client } from "pg";
+
+async function query(queryObject) {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
+  try {
+    return client.query(queryObject);
+  } catch (error) {
+    console.error(error);
+    throw Error("Error querying the database");
+  } finally {
+    await client.end();
+  }
+}
+
+export default {
+  query: query,
+};
