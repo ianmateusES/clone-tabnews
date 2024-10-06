d7a719aecce7bffa673cf4b159fa392d618eccfb
ianmateusES <estudo.ianmateus@gmail.com>
2024-10-06 04:52:59 +0000

    refactor: adjusting `wait-for-postgres` script

--- infra/scripts/wait-for-postgres.js
+++ infra/scripts/wait-for-postgres.js
@@ -1,12 +1,15 @@
-const { exec } = require("node:child_process");
+require('dotenv').config();
 
-function checkPostgres() {
+const { exec } = require("node:child_process");
+const { Client } = require('pg');
+
+function checkPostgresDocker() {
   exec("docker exec postgres-dev pg_isready --host localhost", handleReturn);
 
   function handleReturn(error, stdout) {
     if (stdout.search("accepting connections") === -1) {
       process.stdout.write(".");
-      checkPostgres();
+      checkPostgresDocker();
       return;
     }
 
@@ -14,5 +17,29 @@ function checkPostgres() {
   }
 }
 
+async function checkPostgresClient() {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
+
+  try {
+    await client.connect();
+  } catch (err) {
+   process.stdout.write(".");
+   checkPostgresClient();
+  } finally {
+    await client.end();
+  }
+}
+
 process.stdout.write("\n\n🔴 Aguardando Postgres aceitar conexões");
-checkPostgres();
+
+if (process.env.POSTGRES_HOST !== "localhost") {
+  checkPostgresClient();
+} else {
+  checkPostgresDocker();
+}
